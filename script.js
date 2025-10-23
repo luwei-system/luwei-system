6776c2690fa456b133773fa457a4a8609051f57a
luwei-system <luweisystem@gmail.com>
2025-10-23 19:25:07 +0900

    고정된 네비게이션 제거 및 한영 변환 기능 완전 수정
    - position: fixed 제거하여 네비게이션이 고정되지 않도록 수정
    - 한영 변환에서 브랜드 톤 그리드 제외 조건 제거
    - 모든 data-ko/data-en 속성을 가진 요소가 언어 전환되도록 수정
    - 버전 파라미터를 ?v=20241219-nav-fix로 업데이트
    - 디버깅 로그 유지하여 문제 추적 가능

--- script.js
+++ script.js
@@ -312,7 +312,8 @@
         function updateLanguage(lang) {
             console.log('Updating language to:', lang);
             
-            const elements = document.querySelectorAll('[data-ko][data-en]:not(.brand-tone-grid)');
+            // Update all elements with data-ko and data-en attributes
+            const elements = document.querySelectorAll('[data-ko][data-en]');
             console.log('Found elements to update:', elements.length);
             
             elements.forEach(element => {
